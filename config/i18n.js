315f63b3b2edb0f211646752648a92a7886d1279
Sennett Lau <laub1199@gmail.com>
2020-08-01 15:21:07 +0800

    documentation

---
+++ config/i18n.js
@@ -0,0 +1,31 @@
+module.exports = {
+    locales: [{
+        name: 'Traditional Chinese',
+        code: 'hk',
+        iso: 'zh_HK',
+        file: 'hk.js'
+        },
+        {
+        name: 'Simplified Chinese',
+        code: 'cn',
+        iso: 'zh_CN',
+        file: 'cn.js'
+        },
+        {
+        name: 'English',
+        code: 'en',
+        iso: 'en-US',
+        file: 'en.js'
+        }
+    ],
+    langDir: 'lang/',
+    defaultLocale: 'en',
+    lazy: true,
+    vuex: {
+        moduleName: 'i18n',
+        syncLocale: true,
+        syncMessages: false,
+        syncRouteParams: true
+    },
+    seo: true
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 const pkg = require('./package')
-
+const i18nConfig = require('./config/i18n')
 module.exports = {
   mode: 'spa',
 
@@ -50,38 +50,7 @@ module.exports = {
   */
   modules: [
     '@nuxtjs/vuetify',
-    ['nuxt-i18n', {
-      locales: [
-        {
-          name: 'Traditional Chinese',
-          code: 'hk',
-          iso: 'zh_HK',
-          file: 'hk.js'
-        },
-        {
-          name: 'Simplified Chinese',
-          code: 'cn',
-          iso: 'zh_CN',
-          file: 'cn.js'
-        },
-        {
-          name: 'English',
-          code: 'en',
-          iso: 'en-US',
-          file: 'en.js'
-        }
-      ],
-      langDir: 'lang/',
-      defaultLocale: 'en',
-      lazy: true,
-      vuex: {
-        moduleName: 'i18n',
-        syncLocale: true,
-        syncMessages: false,
-        syncRouteParams: true
-      },
-      seo: true
-    }]
+    ['nuxt-i18n', i18nConfig]
   ],
 
   /*
