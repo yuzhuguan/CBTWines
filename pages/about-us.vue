0cbec1847729047022508ded11a317f442a70bbb
Sennett Lau <laub1199@gmail.com>
2020-08-04 23:44:19 +0800

    new about us page set up

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,5 +1,27 @@
 <template>
-  <v-container class="pt-12" fluid>
+  <div class="about-us">
+    <div class="main-container">
+      <div class="left-block">
+        <div class="top-block"/>
+        <div class="block">
+          <div class="title-block">
+            <h1>{{ $t('about-us.title') }}</h1>
+          </div>
+          <div class="content">
+            <p v-html="$t('about-us.paragraph1')"/>
+            <p v-html="$t('about-us.paragraph2')"/>
+            <nuxt-link to="/">
+              {{ $t('about-us.link') }}
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="right-block">
+        <img src="/media/about-us.jpeg" alt="image">
+      </div>
+    </div>
+  </div>
+  <!-- <v-container class="pt-12" fluid>
     <v-card class="transparent mt-11" flat id="goAboutUs">
       <v-row>
         <v-col :cols="12" lg="6" xl='6'>
@@ -21,16 +43,14 @@
         </v-col>
       </v-row>
     </v-card>
-  </v-container>
+  </v-container> -->
 </template>
 
 <script>
-import VideoBg from 'vue-videobg'
 
 export default {
-  layout: 'vidbg',
+  layout: 'revamp',
   components: {
-    VideoBg
   },
   data() {
     return {
@@ -43,12 +63,67 @@ export default {
 }
 </script>
 
-<style>
-a {
-  text-decoration: none;
-}
-
-.links {
-  color: #a65053;
+<style lang="scss">
+.about-us {
+  width: 100%;
+  height: 89vh;
+  .main-container {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    .left-block {
+      height: 100%;
+      flex-basis: 50%;
+      background: #fafafa;
+      .top-block {
+        width: 100%;
+        height: 120px;
+        background: #fafafa;
+      }
+      .block {
+        width: 100%;
+        height: 400px;
+        background: #f4f0ef;
+            padding-top: 1px;
+        .title-block {
+          text-align: center;
+          h1 {
+            font-size: 70px;
+            font-weight: 300;
+            margin-top: -50px;
+            text-shadow: -5px 5px 6px #aaaaaa;
+          }
+        }
+        .content {
+          padding: 3% 100px 0px;
+          text-align: center;
+          p{
+            font-size: 19px;
+            line-height: 2.0rem;
+            &:first-child {
+              margin-bottom: 10px;
+            }
+            
+          }
+          a {
+            color: #A44C4F;
+            line-height: 2.0rem;
+            text-decoration: none;
+          }
+        }
+      }
+    }
+    .right-block{
+      height: 100%;
+      flex-basis: 50%;
+      img {
+        object-position: 0% 0;
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
