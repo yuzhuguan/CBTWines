32b2d45dedaadcfd41afd78696e4ef667ae10fc5
Sennett Lau <laub1199@gmail.com>
2020-08-04 23:42:27 +0800

    main for CBTWines3.0

---
+++ components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header">
+    <div class="header-container">
+      <div class="logo-container">
+        <img src="/media/logo.png" alt="logo"/>
+      </div>
+      <div class="navlist">
+        <ul>
+          <li><a href="#home">HOME</a></li>
+          <li><a href="#news">ABOUT US</a></li>
+          <li><a href="#contact">WINE LIST</a></li>
+          <li><a href="#about">NEW ARRIVAL</a></li>
+          <li><a href="#about">SPECIAL OFFER</a></li>
+          <li><a href="#about">CONTACT US</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  .header {
+    width: 100%;
+    height: 70px;
+    background-color: white;
+    .header-container {
+      width: 100%;
+      height: 100%;
+      .logo-container {
+        float: left;
+        height: 100%;
+        width: auto;
+        margin-left: 30px;
+        img {
+          height: 100%;
+          padding-bottom: 6px
+        }
+      }
+      .navlist {
+        float: right;
+        height: 100%;
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0 10px 0 0;
+          overflow: hidden;
+          height: 100%;
+          li {
+            float: left;
+            a {
+              display: block;
+              color: #2b2b2b;
+              text-align: center;
+              padding: 25px 26px;
+              text-decoration: none;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ layouts/revamp.vue
@@ -0,0 +1,84 @@
+<template>
+  <main :class="$i18n.locale">
+    <Header/>
+    <section id="wrapper">
+      <nuxt-child />
+    </section>
+  </main>
+  
+</template>
+
+<script>
+import axios from 'axios';
+import Header from '../components/Header'
+export default {
+  components: {
+    Header
+  },
+  data() {
+    return {
+      loggedIn: false,
+      fab: false
+    }
+  },
+  methods: {
+    changeLang(lang) {
+      if (lang === 'traditionalChinese') {
+        this.$router.push('/hk')
+      }
+      else if (lang === 'simplifiedChinese') {
+        this.$router.push('/cn')
+      }
+      else {
+        this.$router.push('/')
+      }
+    },
+    logout() {
+      localStorage.clear();
+      this.loggedIn = false;
+    },
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset ||   e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop () {
+      this.$vuetify.goTo(0)
+    }
+  },
+  mounted() {
+    if(localStorage.getItem('token')) {
+      axios.get('/api/backstage/auth-with-jwt', {headers: {token: localStorage.getItem('token')}})
+          .then(res => {
+              console.log(res);
+              if(res.status === 200) {
+                this.loggedIn = true;
+              }
+          })
+    }
+  },
+  watch: {
+    $route () {
+      this.loggedIn = false;
+      if (localStorage.getItem('token')) {
+        axios.get('/api/auth-with-jwt', {headers: {token: localStorage.getItem('token')}})
+          .then(
+            res => {
+              console.log(res);
+              if(res.status === 200) {
+                this.loggedIn = true;
+              }
+            },
+            err => {
+              console.log(err);
+            }
+          )
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
