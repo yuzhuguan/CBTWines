efb96fff039849daff8f64f2d88af8736e4e6126
Sennett Lau <laub1199@gmail.com>
2020-08-05 23:34:08 +0800

    mobile responsive styling

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -12,9 +12,9 @@
             <p>{{$t('about-us.paragraph2')}}<nuxt-link to="/">
               {{ $t('about-us.link') }}
             </nuxt-link></p>
-            
           </div>
         </div>
+        <div class="bottom-block"/>
       </div>
       <div class="right-block">
         <img src="/media/about-us.png" alt="image">
@@ -66,26 +66,56 @@ export default {
 <style lang="scss">
 .about-us {
   width: 100%;
-  height: 90vh;
+  height: 83.2vh;
+  @media (max-width: 1000px) {
+    height: auto;
+  }
   .main-container {
     height: 100%;
     width: 100%;
     display: flex;
     flex-direction: row;
+    @media (max-width: 1000px) {
+      flex-direction: column-reverse;
+    }
     .left-block {
       height: 100%;
       flex-basis: 50%;
       background: #fafafa;
+      @media (max-width: 1000px) {
+        padding: 0px 70px;
+      }
+      @media (max-width: 550px) {
+        padding: 0px 50px;
+      }
+      @media (max-width: 375px) {
+        padding: 0px 25px;
+      }
       .top-block {
         width: 100%;
         height: 120px;
         background: #fafafa;
+        @media (max-width: 1000px) {
+          height: 101px;
+        }
+        @media (max-width: 550px) {
+          height: 45px;
+        }
+        @media (max-width: 375px) {
+          height: 35px;
+        }
       }
       .block {
         width: 100%;
         height: 400px;
         background: #f4f0ef;
-            padding-top: 1px;
+        padding-top: 1px;
+        @media (max-width: 1000px) {
+          height: 505px;
+        }
+        @media (max-width: 550px) {
+          height: 380px;
+        }
         .title-block {
           text-align: center;
           h1 {
@@ -93,6 +123,10 @@ export default {
             font-weight: 300;
             margin-top: -43px;
             text-shadow: -3px 0px 8px #bbbbbb;
+            @media (max-width: 550px) {
+              font-size: 40px;
+              margin-top: -25px;
+            }
           }
         }
         .content {
@@ -101,30 +135,90 @@ export default {
           margin-right: auto;
           padding-top: 1%;
           text-align: center;
+          @media (max-width: 700px) {
+            width: 350px;
+          }
+          @media (max-width: 550px) {
+            width: 285px;
+          }
+          @media (max-width: 375px) {
+            width: 222px;
+          }
           p{
             font-size: 19px;
             line-height: 1.8rem;
             &:first-child {
               margin-bottom: 10px;
             }
-            
+            @media (max-width: 1000px) {
+              line-height: 2.35rem;
+            }
+            @media (max-width: 700px) {
+              font-size: 17px;
+            }
+            @media (max-width: 550px) {
+              font-size: 14px;
+              line-height: 1.8rem;
+            }
+            @media (max-width: 375px) {
+              font-size: 10px;
+            }
           }
           a {
             color: #A44C4F;
             line-height: 1.8rem;
             text-decoration: none;
+            @media (max-width: 1000px) {
+              line-height: 2.35rem;
+            }
+            @media (max-width: 550px) {
+              font-size: 14px;
+              line-height: 1.8rem;
+            }
+            @media (max-width: 375px) {
+              font-size: 10px;
+            }
           }
+        }
+      }
+      .bottom-block {
+        display: none;
+        @media (max-width: 1000px) {
+          background: #fafafa;
+          height: 75px;
+          display: block;
+        }
+        @media (max-width: 550px) {
+          height: 40px;
+        }
+        @media (max-width: 375px) {
+          height: 20px;
         }
       }
     }
     .right-block{
       height: 100%;
       flex-basis: 50%;
+      @media (max-width: 1000px) {
+        height: 520px;
+      }      
+      @media (max-width: 700px) {
+        height: 400px;
+      }
+      @media (max-width: 375px) {
+        height: 210px;
+      }
       img {
         object-position: 0% 0;
         object-fit: cover;
         height: 100%;
         width: 100%;
+        @media (max-width: 1000px) {
+          object-position: 0% 22%;
+        }
+        @media (max-width: 375px) {
+          object-position: 0% 26%;
+        }
       }
     }
   }
