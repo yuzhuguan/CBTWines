191e7a09ac76d3ed87491d1709615f2c9d171ce4
Sennett Lau <laub1199@gmail.com>
2021-03-04 22:49:09 +0800

    wine-list bg, data table fix

--- pages/wine-list.vue
+++ pages/wine-list.vue
@@ -1,65 +1,67 @@
 <template>
   <div class="wine-list">
     <h1 class="page-title">{{ $t('wine-list.title') }}</h1>
-    <v-row class="content">
-      <v-col class="regions" :cols="2">
-        <ul>
-          <li class="title">{{ $t('wine-list.region') }}</li>
-          <li
-            v-for="(origin, i) in $t('wine-list.origins')"
-            :key="i"
-            @click="changeOrigin(origin.name)"
-            class="region-tag"
-          >{{ origin.displayname }}</li>
-        </ul>
-      </v-col>
-      <v-col>
-        <v-card flat>
-          <v-card-title>
-            <v-text-field
-              v-model="search"
-              append-icon="search"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-          </v-card-title>
-          <v-layout column>
-            <v-flex style="overflow: auto">
-              <v-data-table
-                :headers="$t('wine-list.headers')"
-                :items="wines"
-                :items-per-page="10"
-                :search="search"
-                :sort-by="['name', 'vintage']"
-              >
-                <template v-slot:item.name="{ item }">
-                  <p :class="item.colorName" style="margin: 0">{{item.name}}</p>
-                </template>
-                <template v-slot:item.chineseName="{ item }">
-                  <p :class="item.colorChineseName" style="margin: 0">{{item.chineseName}}</p>
-                </template>
-                <template v-slot:item.origin="{ item }">
-                  <p :class="item.colorOrigin" style="margin: 0">{{item.origin}}</p>
-                </template>
-                <template v-slot:item.appellation="{ item }">
-                  <p :class="item.colorAppellation" style="margin: 0">{{item.appellation}}</p>
-                </template>
-                <template v-slot:item.vintage="{ item }">
-                  <p :class="item.colorVintage" style="margin: 0">{{item.vintage}}</p>
-                </template>
-                <template v-slot:item.rating="{ item }">
-                  <p :class="item.colorRating" style="margin: 0">{{item.rating}}</p>
-                </template>
-                <template v-slot:item.price="{ item }">
-                  <p :class="item.colorPrice" style="margin: 0">{{item.price}}</p>
-                </template>
-              </v-data-table>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-app>
+      <v-row class="content">
+        <v-col class="regions" :cols="2">
+          <ul>
+            <li class="title">{{ $t('wine-list.region') }}</li>
+            <li
+              v-for="(origin, i) in $t('wine-list.origins')"
+              :key="i"
+              @click="changeOrigin(origin.name)"
+              class="region-tag"
+            >{{ origin.displayname }}</li>
+          </ul>
+        </v-col>
+        <v-col>
+          <v-card flat>
+            <v-card-title>
+              <v-text-field
+                v-model="search"
+                append-icon="search"
+                label="Search"
+                single-line
+                hide-details
+              ></v-text-field>
+            </v-card-title>
+            <v-layout column>
+              <v-flex style="overflow: auto">
+                <v-data-table
+                  :headers="$t('wine-list.headers')"
+                  :items="wines"
+                  :items-per-page="10"
+                  :search="search"
+                  :sort-by="['name', 'vintage']"
+                >
+                  <template v-slot:item.name="{ item }">
+                    <p :class="item.colorName" style="margin: 0">{{item.name}}</p>
+                  </template>
+                  <template v-slot:item.chineseName="{ item }">
+                    <p :class="item.colorChineseName" style="margin: 0">{{item.chineseName}}</p>
+                  </template>
+                  <template v-slot:item.origin="{ item }">
+                    <p :class="item.colorOrigin" style="margin: 0">{{item.origin}}</p>
+                  </template>
+                  <template v-slot:item.appellation="{ item }">
+                    <p :class="item.colorAppellation" style="margin: 0">{{item.appellation}}</p>
+                  </template>
+                  <template v-slot:item.vintage="{ item }">
+                    <p :class="item.colorVintage" style="margin: 0">{{item.vintage}}</p>
+                  </template>
+                  <template v-slot:item.rating="{ item }">
+                    <p :class="item.colorRating" style="margin: 0">{{item.rating}}</p>
+                  </template>
+                  <template v-slot:item.price="{ item }">
+                    <p :class="item.colorPrice" style="margin: 0">{{item.price}}</p>
+                  </template>
+                </v-data-table>
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-app>
     <div class="mobile-content">
       <ul>
         <li
@@ -146,7 +148,8 @@ export default {
 
 <style lang="scss">
 .wine-list {
-  background: url("https://res.cloudinary.com/dinad7iot/image/upload/v1612189163/cbtwines/winelist_yhrumg.jpg");
+  background: url("https://res.cloudinary.com/dinad7iot/image/upload/c_crop,e_blur:231,o_63,q_auto/v1612189163/cbtwines/winelist_yhrumg.jpg") no-repeat center;
+  background-size: cover;
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -156,16 +159,37 @@ export default {
     background: none;
   }
   h1 {
-    padding-left: 44px;
-    font-size: 30px;
     padding-top: 8px;
+    margin: 3vh 0 0px;
     color: #A44c4f;
+    text-align: center;
+    font-size: 4rem;
     @media (max-width: 1050px) {
       padding-left: 0;
       text-align: center;
       margin: 20px 0;
     }
   }
+  #app {
+    background: none;
+    max-width: 97%;
+    width: 97%;
+    margin: auto;
+    border-radius: 8px;
+    @media (max-width: 1050px) {
+      display: none;
+    }
+    .v-application--wrap {
+      min-height: 0;
+      .v-data-table__wrapper {
+        height: 555px;
+        max-height: 555px;
+        overflow-y: auto;
+      }
+    }
+
+  }
+
   .content {
     max-width: 100%;
     @media (max-width: 1050px) {
@@ -199,6 +223,8 @@ export default {
     border-radius: 8px;
     margin-top: 9px;
     margin-left: 26px;
+    max-height: 700px;
+    overflow: auto;
     ul {
       list-style: none;
       li {
@@ -214,25 +240,26 @@ export default {
     .region-tag {
       cursor: pointer;
     }
+
+    &::-webkit-scrollbar {
+      height: 10px;
+      width: 10px;
+      background-color: transparent;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: rgb(146, 145, 145);
+      -webkit-border-radius: 5px;
+      -moz-border-radius: 5px;
+      -ms-border-radius: 5px;
+      -o-border-radius: 5px;
+      border-radius: 5px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
   }
   a {
     text-decoration: none;
-  }
-  .scrollable::-webkit-scrollbar {
-    height: 10px;
-    width: 10px;
-    background-color: transparent;
-  }
-  .scrollable::-webkit-scrollbar-thumb {
-    background-color: rgb(146, 145, 145);
-    -webkit-border-radius: 5px;
-    -moz-border-radius: 5px;
-    -ms-border-radius: 5px;
-    -o-border-radius: 5px;
-    border-radius: 5px;
-  }
-  .scrollable::-webkit-scrollbar-track {
-    background-color: transparent;
   }
   .mobile-content {
     display: none;
